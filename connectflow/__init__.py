f6b545203317748a1cbf7abc4e1517dff87b82ab
vjohs <vjohs@icloud.com>
2022-05-04 11:24:17 +0900

    core(base/index) views add

--- connectflow/__init__.py
+++ connectflow/__init__.py
@@ -5,3 +5,16 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 app = Flask(__name__)
+
+
+
+
+
+
+
+
+#########################
+#### BLUEPRINT SETUP ####
+#########################
+from connectflow.core.views import core
+app.register_blueprint(core)
