a431d54d4976e4394fc50d8b5c0edc9cfa9da11b
vjohs <vjohs@icloud.com>
2022-05-07 19:59:34 +0900

    models.py 수정

--- connectflow/networks/models.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, url_for, flash, redirect, request, Blueprint
-
-netowrks = Blueprint('netowrks', __name__)
-
-@netowrks.route('/netowrks')
-def find(): 
-    return '<h1>netowrks</h1>'
